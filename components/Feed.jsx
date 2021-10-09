483c4a4836590bad75ef64e3b285febd71ee0ec4
fadhilsaheer <fadhilsaheer@gmail.com>
2021-10-09 05:51:08 +0530

    📫 posts

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Stories from './Stories'
 import InputBox from './InputBox'
+import Posts from './Posts'
 
-export default function Feed() {
+export default function Feed({ posts }) {
   return (
     <div className='flex flex-grow h-screen pb-4 pt-6 mr-4 xl:mr-40 overflow-y-auto scrollbar-hide mb-200'>
       <div className='mx-auto max-w-sm md:max-w-md lg:max-w-xl xl:max-w-xl'>
         <Stories />
         <InputBox />
-        {/* posts */}
+        <Posts posts={posts} />
       </div>
     </div>
   )
